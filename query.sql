4255adbab7e7cb2828d27e6b5c6cd73e43437896
Lander <lander.gerotto@gmail.com>
2023-11-09 13:46:24 -0300

    Query Hotfix

--- query.sql
+++ query.sql
@@ -2,18 +2,18 @@ use master
 go
 
 if (exists(select * from sys.databases where name = 'McDataBase'))
-	drop database McDatabase
+	drop database McDataBase
 go
 
 create database McDataBase
 go
 
-use McDatabase
+use McDataBase
 go
 
 create table Product(
 	ID int identity primary key,
-	ProductName varchar(200) not null,
+	ProductName varchar(80) not null,
 	Photo varbinary(MAX) null,
 	DescriptionText varchar(400) not null
 );
@@ -21,13 +21,13 @@ go
 
 create table Store(
 	ID int identity primary key,
-	Localization varchar(200) not null,
+	Localization varchar(200) not null
 );
 go
 
 create table MenuItem(
 	ID int identity primary key,
-	ProductId int references Product(ID) not null,
+	ProductID int references Product(ID) not null,
 	StoreID int references Store(ID) not null,
 	Price decimal(5, 2) not null
 );
@@ -40,8 +40,9 @@ create table ClientOrder(
 	Moment datetime not null,
 	DeliveryMoment datetime null
 );
+go
 
-create table ClientOrderItem (
+create table ClientOrderItem(
 	ID int identity primary key,
 	ClientOrderID int references ClientOrder(ID) not null,
 	ProductID int references Product(ID) not null
